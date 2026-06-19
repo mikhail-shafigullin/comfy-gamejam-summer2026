0acc15121c2b2d62205cc23b56df4091fe423af0
AltTabber <mikeice414@gmail.com>
2026-06-19 08:15:07 +0200

    add recipe book

--- levels/RecipeScreen.gd
+++ levels/RecipeScreen.gd
@@ -1,11 +1,40 @@
 extends Control
 
+@onready var bookMohito: Sprite2D = %BookMohito;
+@onready var bookDaikiri: Sprite2D = %BookDaikiri;
+@onready var bookAnanaslimonad: Sprite2D = %BookAnanaslimonad;
+@onready var bookBauntibriz: Sprite2D = %BookBauntibriz;
+@onready var bookPinocolada: Sprite2D = %BookPinocolada;
+@onready var bookAnimSprite: AnimatedSprite2D = %BookAnimSprite;
+@onready var subViewportContainer: SubViewportContainer = %SubViewportContainer;
+var books: Array[Sprite2D];
+var currentIndex = 0;
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	subViewportContainer.gui_input.connect(_onGuiInput);
+	books = [bookMohito, bookDaikiri, bookAnanaslimonad, bookBauntibriz, bookPinocolada];
+
+func _onGuiInput(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		nextBook();
+
+func nextBook():
+	clearBook();
+	currentIndex += 1;
+	if(currentIndex >= books.size()):
+		currentIndex = 0;
+	bookAnimSprite.play("changePage");
+	bookAnimSprite.animation_finished.connect(func(): books[currentIndex].show(), CONNECT_ONE_SHOT);
+
+func prevBook():
+	clearBook();
+	currentIndex -= 1;
+	if(currentIndex <= -1):
+		currentIndex = books.size() - 1;
+	bookAnimSprite.play("changePage");
+	bookAnimSprite.animation_finished.connect(func(): books[currentIndex].show(), CONNECT_ONE_SHOT);
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func clearBook():
+	for book in books:
+		book.hide();
